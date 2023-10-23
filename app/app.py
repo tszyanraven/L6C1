e009d56d85b7959510c6018835094fa58f35b4f0
tszyanraven <146828655+tszyanraven@users.noreply.github.com>
2023-10-23 20:03:48 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
-from joblib import load
+import random
 import os
 
 app = Flask(__name__)
@@ -14,10 +14,10 @@ def hello_world():
     else:
         myage = request.form['age']
         mygender = request.form['gender']
-        model = load('app/watch-recommender.joblib')
+        mylist = ["digital","smart","quartz"]
         np_arr = np.array([myage, mygender])
         predictions = model.predict([np_arr])  
         predictions_to_str = str(predictions)
         #return predictions_to_str
-        return render_template('index.html', href2='The suitable watch for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
+        return render_template('index.html', href2='The suitable watch for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'random.choice(mylist))
 
